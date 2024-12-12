aa8d313f34a8b0f856902babad5653275d07d179
Roshan-R <roshanr2001@gmail.com>
2024-12-12 11:39:40 +0530

    feat: add new experimental scrape_headless func

--- src/handlers/workflow/scrape.rs
+++ src/handlers/workflow/scrape.rs
@@ -1,9 +1,10 @@
-use crate::controllers::scraper::scrape;
+use crate::controllers::scraper::{scrape, scrape_headless};
 use crate::handlers::workflow::types::{ScrapeReq, ScrapeResp};
 use actix_web::{web, HttpResponse, Responder};
 
 pub async fn handle_scrape(data: web::Json<ScrapeReq>) -> impl Responder {
     let selected: String = scrape(&data.url, &data.selector).await.unwrap();
+    //let selected: String = scrape_headless(&data.url, &data.selector).await.unwrap();
     if selected.is_empty() {
         return HttpResponse::NotFound().body("Could not scrape");
     }
